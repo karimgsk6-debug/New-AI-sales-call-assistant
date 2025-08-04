72c9a561a8c762f02f5adf865a9e6968997cb622
karimgsk6-debug <karim.gsk6@gmail.com>
2025-08-04 20:12:12 +0300

    Update ai_sales_assistant_app.py

--- ai_sales_assistant_app.py
+++ ai_sales_assistant_app.py
@@ -1,4 +1,14 @@
+# old
+import openai
 
+openai.api_key = os.environ['OPENAI_API_KEY']
+
+# new
+from openai import OpenAI
+
+client = OpenAI(
+  api_key=os.environ['OPENAI_API_KEY'],  # this is also the default, it can be omitted
+)
 import streamlit as st
 import openai
 
