cd60c02ac63081b6527da9fa8fc3a834a301ac3c
karimgsk6-debug <karim.gsk6@gmail.com>
2025-08-19 15:18:18 +0300

    Update TestV13 per brand.py

--- TestV13 per brand.py
+++ TestV13 per brand.py
@@ -24,7 +24,7 @@ gsk_brands = {
 
 # --- Brand logos (mix of local and URL) ---
 gsk_brands_images = {
-    "Augmentin": "https://tovpet.com/cdn/shop/products/16038589266422789.png?v=1736934091",
+    "Augmentin": "https://www.bloompharmacy.com/cdn/shop/products/augmentin-1-gm-14-tablets-145727_600x600_crop_center.jpg?v=1687635056",
     "Shingrix": "https://www.oma-apteekki.fi/WebRoot/NA/Shops/na/67D6/48DA/D0B0/D959/ECAF/0A3C/0E02/D573/3ad67c4e-e1fb-4476-a8a0-873423d8db42_3Dimage.png",
     "Seretide": "images/seretide.png",
 }
